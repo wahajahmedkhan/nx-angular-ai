de8f96f6b9ca541070ae2ebd346ff4b5253f2f1f
Sheikh Taha <58697651+Taha0528@users.noreply.github.com>
2025-03-09 18:26:43 +0500

    sign-in or sign-up ke component bana liye

---
+++ src/app/components/auth/sign-in/sign-in.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-sign-in',
+  imports: [CommonModule],
+  templateUrl: './sign-in.component.html',
+  styleUrl: './sign-in.component.css',
+})
+export class SignInComponent {}
