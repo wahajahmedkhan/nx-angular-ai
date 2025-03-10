e594ae4e6981cca8ebe0636e027006bdc9586d7e
Sheikh Taha <58697651+Taha0528@users.noreply.github.com>
2025-03-10 15:52:59 +0500

    new sign-in design

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,49 @@
 import { Route } from '@angular/router';
+import { SignInComponent } from './components/auth/sign-in/sign-in.component';
+import { SignUpComponent } from './components/auth/sign-up/sign-up.component';
 import { ChatLayoutComponent } from './components/chat-layout/chat-layout.component';
 
 export const appRoutes: Route[] = [
+
+  { 
+    path: '', 
+    redirectTo: 'sign-in', 
+    pathMatch: 'full'
+
+   }, 
+
+  { 
+    path: 'sign-in', 
+    component: SignInComponent 
+  },
+
+  { 
+    path: 'sign-up', 
+    component: SignUpComponent 
+  },
+
+  { 
+    path: '**', 
+    redirectTo: 'sign-in' 
+  }, 
+  
   {
     path: '',
-    component: ChatLayoutComponent
+    component: ChatLayoutComponent,
+    children: [
+      {
+        path: 'chat-layout',
+        component: ChatLayoutComponent
+      },
+            
+    ]
   },
-  {
-    path: '**',
-    redirectTo: ''
-  }
+  // {
+  //   path: '',
+  //   component: ChatLayoutComponent
+  // },
+  // {
+  //   path: '**',
+  //   redirectTo: ''
+  // }
 ];
