8f3948802a89d17cff08b77d2d31afe54b401a97
wahajahmedkhan <wahajahmedkhan95@gmail.com>
2025-04-23 04:54:34 +0500

    Update API URLs to use relative paths instead of absolute URLs

--- src/environment.ts
+++ src/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  FLOWWISE_API_URL: 'http://akudeb.itserver.biz:3000/api/v1/prediction/a180e12e-d360-47fd-9fbd-443dcc3a5d1d',
-  AUTH_API_URL: 'http://akudeb.itserver.biz:3003/api/auth'
+  FLOWWISE_API_URL: '/api/v1/prediction/a180e12e-d360-47fd-9fbd-443dcc3a5d1d',
+  AUTH_API_URL: '/api/auth'
 };
